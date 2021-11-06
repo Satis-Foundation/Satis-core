9f6122e2e80ec0875936a6b18acfd3a539d106d1
Enoch NG <enoch.ng@sat.is>
2021-11-06 14:05:06 +0800

    multisig withdraw demo for issue #28

---
+++ multisigWithdraw_demo/test/test_5a_removeMoreThanPossible.js
@@ -0,0 +1,60 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { getContractAddress } = require('@ethersproject/address');
+
+
+
+describe ("Check remove fund more than possible", function() {
+    it ("Remove more than possible", async function() {
+
+        const signers = await ethers.getSigners();
+        console.log("Number of accounts: " + signers.length);
+
+
+        const tokenOwner = signers[0];
+        tokenOwnerAddress = tokenOwner['address'];
+        console.log("Token owner address: " + tokenOwnerAddress);
+
+        const tokenContract = await ethers.getContractFactory("Token", tokenOwner);
+        const token = await tokenContract.deploy();
+        console.log("Token contract deployed");
+
+        const tokenAddress = token.address;
+        console.log("Token address: " + tokenAddress);
+
+
+        const poolOwner = signers[1];
+        poolOwnerAddress = poolOwner['address'];
+        console.log("Money Pool owner address: " + poolOwnerAddress);
+
+        const poolContract = await ethers.getContractFactory("moneyPoolL2", poolOwner);
+        const pool = await poolContract.deploy();
+        console.log("Pool contract deployed");
+
+        const poolAddress = pool.address;
+        console.log("Money Pool address: " + poolAddress);
+
+        const user0 = signers[0];
+        const user1 = signers[1];
+        const user2 = signers[2];
+        console.log("User 0 address: " + user0['address']);
+        console.log("User 1 address: " + user1['address']);
+        console.log("User 2 address: " + user2['address']);
+
+        await token.connect(user0).transfer(user1['address'],100);
+        await token.connect(user0).transfer(user2['address'],100);
+
+        await token.connect(user2).approve(poolAddress,100);
+        await pool.connect(user2).addFund(tokenAddress,100);
+        await pool.connect(user2).lockFundWithAction(tokenAddress,58,'lockTest');
+        balanceValue = await pool.connect(user2).viewFund(tokenAddress);
+        expect (balanceValue[0]).to.equal(100);
+        expect (balanceValue[1]).to.equal(58);
+
+        await pool.connect(user2).removeFund(tokenAddress,70);
+        balanceValue = await pool.connect(user2).viewFund(tokenAddress);
+        expect (balanceValue[0]).to.equal(30);
+        expect (balanceValue[1]).to.equal(58);
+
+    })
+})
