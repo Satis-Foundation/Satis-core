bcd7cea1891cbb8d956c4ff63e6454b9f58babba
John Lee <john.lee@sat.is>
2022-01-05 08:59:18 +0800

    Add L2 networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,29 +1,70 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-require('@nomiclabs/hardhat-waffle')
-require('@eth-optimism/hardhat-ovm')
+
+require('hardhat-typechain');
+require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
 
 module.exports = {
-
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
   solidity: {
-    compilers: [{
-      version: "0.8.0"
-    }, {
-      version: "0.4.17"
-    },]
+    version: '0.8.0',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+      metadata: {
+        // do not include the metadata hash, since this is machine dependent
+        // and we want all generated code to be deterministic
+        // https://docs.soliditylang.org/en/v0.7.6/metadata.html
+        bytecodeHash: 'none',
+      },
+    },
   },
-  ovm: {
-    solcVersion: "0.7.6",
-  },
-
   networks: {
-    "optimistic-kovan": {
-      url: 'https://kovan.optimism.io',
-      accounts: ['0x3cdb6a0cf7b1883b96e7c57896c7812775cbca6f51fbf0b52073db05fca16875'],
-      gasPrice: 15000000,
-      ovm: true,
-    }
-  }
+    hardhat: {
+      allowUnlimitedContractSize: false,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    arbitrumRinkeby: {
+      url: `https://arbitrum-rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    arbitrum: {
+      url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    optimismKovan: {
+      url: `https://optimism-kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    optimism: {
+      url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+  },
 
 };
