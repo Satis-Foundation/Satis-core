32cdfdf76b56f7f6cba1cfb577e7af1f7ee9b038
Enoch Ng <enoch.ng@sat.is>
2023-03-13 18:00:03 +0800

    phase out sigmaPoolRaw.sol

--- contracts/v2/sigma_mining/SigmaPoolProxy.sol
+++ contracts/v2/sigma_mining/SigmaPoolProxy.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-//import "../lib_and_interface/ISigmaPoolRaw.sol";
 import "../lib_and_interface/IMoneyPoolRaw.sol";
 import "../lib_and_interface/ISigmaAction.sol";
 import "../lib_and_interface/Address.sol";
