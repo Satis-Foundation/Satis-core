b3ba4111ad0bd6a2de40d91596ed42ab1620bb92
Enoch Ng <enoch.ng@sat.is>
2023-07-03 17:06:43 +0800

    update script

--- testnet-hardhat-case/hardhat.config.js
+++ testnet-hardhat-case/hardhat.config.js
@@ -27,7 +27,7 @@ module.exports = {
     },
     goerli: {
       url: "https://rpc.ankr.com/eth_goerli",
-      accounts: ["34590ce513a0fc9d84fe3b0213071e8a5493f184b766769b81d363860ee72879", "aaaf8ea96000fb58e5b9693ad0fc7d6fac499c84762dc678a43012d712cf34fe", "0x1887177de76c422c7de396c993d357b3ab512b0fd21db9fc182829df31425468"],
+      accounts: ["", "", ""],
       allowUnlimitedContractSize: false,
     },
   },
