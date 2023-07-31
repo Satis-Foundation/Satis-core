59e1ea32b6b333d15edf94a2af5bfacff17939c7
Enoch Ng <enoch.ng@sat.is>
2023-07-31 15:05:05 +0800

    event fix

--- contracts/v2/lib_and_interface/ISigmaAction.sol
+++ contracts/v2/lib_and_interface/ISigmaAction.sol
@@ -17,7 +17,7 @@ interface ISigmaAction {
     /**
      * @dev Emit withdraw fund event
      */
-    function sigmaWithdrawFund(string memory _ticketId, address _clientAddress, address _tokenAddress, uint256 _tokenValue) external returns(bool);
+    function sigmaWithdrawFund(string memory _ticketId, address _clientAddress, address _tokenAddress, uint256 _withdrawValue, uint256 _inDebtValue) external returns(bool);
 
     /**
      * @dev Emit redeem fund event
