db94334103a942b7efaee37b98b022055b067cc8
Enoch Ng <enoch.ng@sat.is>
2023-08-21 10:48:34 +0800

    update interface for zk

--- zksync-hardhat-case/hardhat.config.ts
+++ zksync-hardhat-case/hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
+import "@matterlabs/hardhat-zksync-verify";
 
 // dynamically changes endpoints for local tests
 const zkSyncTestnet =
@@ -12,9 +13,13 @@ const zkSyncTestnet =
         zksync: true,
       }
     : {
+        //url: "https://mainnet.era.zksync.io",
         url: "https://zksync2-testnet.zksync.dev",
+        //ethNetwork: "",
         ethNetwork: "goerli",
         zksync: true,
+        //verifyURL: 'https://zksync2-mainnet-explorer.zksync.io/contract_verification',
+        verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification',
       };
 
 const config: HardhatUserConfig = {
