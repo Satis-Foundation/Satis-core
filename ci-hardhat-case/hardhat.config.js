72ec78047b7a50c58a0f17237c49f569dc987a8d
Enoch Ng <enoch.ng@sat.is>
2023-12-07 16:16:40 +0800

    add ci

---
+++ ci-hardhat-case/hardhat.config.js
@@ -0,0 +1,30 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-ethers');
+require('@typechain/hardhat');
+require('@nomiclabs/hardhat-etherscan');
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 800,
+          },
+          metadata: {
+            bytecodeHash: 'none',
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+  },
+};
+
